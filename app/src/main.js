df45d71e35ffcc9463276bd034513c5c7b9e8230
Daniel <danield494@nycstudents.net>
2025-11-13 11:00:30 -0800

    1

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,48 @@
 import './style.css'
-const Physics = [
-  "The"
+const music = [
+    {
+    Name:"Do I Wanna Know ?",
+    Artist: "Arctic Monkeys",
+    Genre: "Indie Rock",
+  },
+    {
+    Name:"I Wanna Be Yours",
+    Artist:"Arctic Monkeys",
+    Genre:"Indie Rock",
+  },
+    {
+    Name:"Anabella",
+    Artist:"Arctic Monkeys",
+    Genre:"Indie Rock",
+  },
+    {
+    Name:"Believer",
+    Artist:"Imagine Dragons",
+    Genre:"Pop Rock",
+  },
+    {
+    Name:"Thunder",
+    Artist:"Imagine Dragons",
+    Genre:"Pop Rock",
+  },
+    {
+    Name:"Radioactive",
+    Artist:"Imagine Dragons",
+    Genre:"",
+  },
+    {
+    Name:"Shake It Off",
+    Artist:"Taylor Swift",
+    Genre:"Pop Rock",
+  },
+    {
+    Name:"Love Story",
+    Artist:"Taylor Swift",
+    Genre:"Pop Rock",
+  },
+    {
+    Name:"Cruel Summer",
+    Artist:"Taylor Swift",
+    Genre:"Pop Rock",
+  },
 ]
